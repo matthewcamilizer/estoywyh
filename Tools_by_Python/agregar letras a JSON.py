af408c1d9edb62d7221b2bef7bba1fb1b3d06823
matthewcamilizer <matthewhilmi666@gmail.com>
2023-04-26 05:51:48 +0800

    u

---
+++ Tools_by_Python/agregar letras a JSON.py
@@ -0,0 +1,23 @@
+import os
+
+drive = input("input the path: ")
+
+for root, dirs, files in os.walk(drive):
+    for filename in files:
+        if filename.endswith(".txt"):
+            filepath = os.path.join(drive,filename)
+            with open(filepath, 'r', encoding = 'iso-8859-1') as f:
+                lines = f.readlines()
+                ##read all lines in the file and return them as a list
+                lyrics = ''.join(lines)
+                formatted_lyrics ='\n'.join(["\t" + fr'''{{"start":"", "end":"" , "text": "{line.strip()}"}},''' for line in lyrics.split('\n')])
+
+                base_name, _ = os.path.splitext(filename)
+                jsoutput = os.path.join(drive, f"{base_name}.json")
+                with open(jsoutput, "w", encoding = 'iso-8859-1') as ff:
+                    ff.write("{" + fr'''"{base_name}":''' + '\n' +'\t' + "{" + fr'''"lyrics": [
+    {formatted_lyrics}
+]'''+ "}" + "}")
+
+
+
