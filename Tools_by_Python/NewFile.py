89671eff6797c0936bbf0601d8165d7c25fe016a
matthewcamilizer <matthewhilmi666@gmail.com>
2023-05-25 06:16:25 +0800

    u

---
+++ Tools_by_Python/NewFile.py
@@ -0,0 +1,12 @@
+import os
+
+def newfile(path, filename):
+    base, extension = os.path.splitext(filename)
+    counter = 1
+    new_filename = os.path.join(path,filename)
+
+    while os.path.exists(new_filename):
+        new_filename = f"{os.path.join(path,base)}({counter}){extension}"
+        counter += 1
+    return new_filename
+
