c681287469f71a36893ddf6f730cc37b8102cf4a
matthewcamilizer <50293364+matthewcamilizer@users.noreply.github.com>
2023-05-26 16:56:38 +0800

    u

--- Tools_by_Python/se.py
+++ Tools_by_Python/se.py
@@ -8,7 +8,14 @@ aa=win32api.GetLogicalDriveStrings()
 a=aa.split('\000')[:-1]
 enter=input("输入忘记位置的文件: ")
 file, fmt = os.path.splitext(enter)
-p=r"C:\Users\Administrator\Desktop"
+p=input(r"输入保存日志的路径(如果什么都不输入就直接按回车, 默认保存在桌面, 路径输错了也保存在桌面):")
+ep=''
+if not p or not os.path.exists(p):
+	if not os.path.exists(p):
+		ep=p
+		print(f"你输入的这个路径{p}电脑里没有, 日志默认保存在桌面")
+	p=r"C:\Users\Administrator\Desktop"
+
 log=newfile(p, f"{ff}找文件 \u2022 '{file}'.txt")
 
 found=[]
@@ -23,8 +30,10 @@ for ss in a:
 				with open (log, 'a', encoding='utf-8') as lf:
 					if lf.tell()==0:
 						lf.write(f"{ff}\n找这个文件: {enter}\n\n打开方式: 先打开'我的电脑' \n再把每行的文件路径复制粘贴到'我的电脑'上面那个长框框中, 然后按回车\n下面列出的是文件路径\n\n")
+						if not os.path.exists(ep):
+							lf.write(f"你的保存路径输的是这个: {ep} 电脑里没有\n所以默认存在桌面: {p}")
 					here=here.replace("\\\\", "\\")
-					lf.write(f"\n\n{here}\n\n")
+					lf.write(f"\n\n第{len(found)}个可能的 {enter} 文件:\n{here}\n\n")
 
 input(f"一共找到{len(found)}个可能的 {enter} 文件, 按回车退出")
 
