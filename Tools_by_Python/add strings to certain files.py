14be249b9f297de7d488af4b927ba4e79059b07d
matthewcamilizer <50293364+matthewcamilizer@users.noreply.github.com>
2023-05-26 10:00:47 +0800

    u

--- Tools_by_Python/add strings to certain files.py
+++ Tools_by_Python/add strings to certain files.py
@@ -2,11 +2,16 @@ import sys, os
 sys.dont_write_bytecode = True
 
 path=input(r"path: ")
+fmt="."+input("format: ")
+add_change=input("contents to add: ")
+if "\\n" in add_change:
+    add_change = add_change.replace("\\n", os.linesep)
+
 
 for pyf in os.listdir(path):
-    if pyf.endswith('.py'):
+    if pyf.endswith(fmt):
         addfile=os.path.join(path,pyf)
         with open (addfile, 'r+', encoding='utf-8') as f:
             content=f.read()
             f.seek(0,0)
-            f.write("import sys\nsys.dont_write_bytecode = True\n" + content)
+            f.write(add_change + content)
