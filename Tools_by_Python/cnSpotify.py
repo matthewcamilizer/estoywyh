abd8263b1ee2eb24d3451795b79f50faaf4ceba1
matthewcamilizer <50293364+matthewcamilizer@users.noreply.github.com>
2023-06-04 15:27:37 +0800

    1

--- Tools_by_Python/cnSpotify.py
+++ Tools_by_Python/cnSpotify.py
@@ -8,7 +8,7 @@ from NewFile import newfile
 current_datetime = datetime.datetime.now()
 ff=current_datetime.strftime('%Y-%m-%d')
 
-def ExportSpotify(ttp, Enterpath):
+def ExportSpotify(ttp, Enterpath, Spotify_export, Failed_export):
     #ttp = input(r"Enter Spotify URL here: ")
     #Enterpath = input(r"Enter path to save log or press ENTER to skip: ")
     client_id = "348de570cabd4bbdabb3eb80b2be3b54"
@@ -17,11 +17,10 @@ def ExportSpotify(ttp, Enterpath):
     playlist=r"playlist/([^\W]+)"
     album=r"album/([^\W]+)"
     sp=re.search(playlist, ttp)
-    sa=re.search(album, ttp)
     path=tp=""
 
     if Enterpath:
-        exp=os.path.join(Enterpath, "歌单导出")
+        exp=os.path.join(Enterpath, "export")
         if not os.path.exists(exp):
             os.mkdir(exp)
         spexp=os.path.join(exp, "Spotify")
@@ -29,66 +28,66 @@ def ExportSpotify(ttp, Enterpath):
             os.mkdir(spexp)
         path=spexp
 
+    tp = f"{base_tp}playlists/{sp.group(1)}"
+    try:
+        def get_token():
+            auth_str=client_id+":"+client_secret
+            auth_bytes=auth_str.encode("utf-8")
+            auth_64=str(base64.b64encode(auth_bytes),"utf-8")
+
+            u="https://accounts.spotify.com/api/token"
+            h={"Authorization":"Basic "+auth_64,"Content-Type":"application/x-www-form-urlencoded"}
+            d={"grant_type":"client_credentials"}
+            r=post(u,headers=h,data=d)
+            json_result=json.loads(r.content)
+            tk=json_result["access_token"]
+            return tk
+        token = get_token()
+
+        def get_auth_token(tkk):
+            return {"Authorization":"Bearer "+tkk}
+
+        store_track=[]
+        store_song=[]
+        store_artist=[]
+        uris=[]
+        logFile=''
+        title=''
+        author=''
 
 
-    if "playlist" in ttp:
-        tp = f"{base_tp}playlists/{sp.group(1)}"
+        def APIget(ttk):
+            h=get_auth_token(ttk)
+            re=get(tp,headers=h)
 
-    def get_token():
-        auth_str=client_id+":"+client_secret
-        auth_bytes=auth_str.encode("utf-8")
-        auth_64=str(base64.b64encode(auth_bytes),"utf-8")
+            json_re=json.loads(re.content)["tracks"]["items"]
 
-        u="https://accounts.spotify.com/api/token"
-        h={"Authorization":"Basic "+auth_64,"Content-Type":"application/x-www-form-urlencoded"}
-        d={"grant_type":"client_credentials"}
-        r=post(u,headers=h,data=d)
-        json_result=json.loads(r.content)
-        tk=json_result["access_token"]
-        return tk
-    token = get_token()
-
-    def get_auth_token(tkk):
-        return {"Authorization":"Bearer "+tkk}
-
-    store_track=[]
-    store_song=[]
-    store_artist=[]
-    uris=[]
-    logFile=''
-    title=''
-    author=''
-
-
-    def APIget(ttk):
-        h=get_auth_token(ttk)
-        re=get(tp,headers=h)
-        json_re=json.loads(re.content)["tracks"]["items"]
-
-        title=json.loads(re.content)['name']
-        author=json.loads(re.content)['owner']['display_name']
-        for j in json_re:
-            for k,jj in j['track'].items():
-                store_track.append((k,jj))
-        for key, value in store_track:
-            if key=='name':
-                store_song.append(value)
-            if key=='uri':
-                uris.append(value)
-            if key=='artists':
-                artists=[]
-                for jjj in value:
-                    artists.append(jjj['name'])
-                store_artist.append(", ".join(artists))
-        for count, (a,b,c) in enumerate(zip(store_artist,store_song ,uris), start=1):
-            print(f"第{count}首:\n{a} - {b}\n{c}\n\n")
-        if path:
-            logFile=newfile(path, f"Spotify {author}的歌单{title}{ff}.txt")        
-            for count, (artist, song, u) in enumerate(zip(store_artist, store_song, uris),start=1):
-                with open(logFile, 'a', encoding='utf-8') as f:
-                    if f.tell()==0:
-                        f.write(f"{ff}\n作者: {author}\n歌单: {title}\n链接: {ttp}\n\n")
-                    f.write(f"第{count}首:\n{artist} • {song}\n{u}\n\n")
-
-
-    APIget(token)
+            title=json.loads(re.content)['name']
+            author=json.loads(re.content)['owner']['display_name']
+            for j in json_re:
+                for k,jj in j['track'].items():
+                    store_track.append((k,jj))
+            for key, value in store_track:
+                if key=='name':
+                    store_song.append(value)
+                if key=='uri':
+                    uris.append(value)
+                if key=='artists':
+                    artists=[]
+                    for jjj in value:
+                        artists.append(jjj['name'])
+                    store_artist.append(", ".join(artists))
+            for count, (a,b,c) in enumerate(zip(store_artist,store_song ,uris), start=1):
+                print(f"Number: {count}\n{a} - {b}\n{c}\n\n")
+                Spotify_export.append(f"{a} \u2022 {b}")
+            if path:
+                logFile=newfile(path, f"Spotify {author} • {title}{ff}.txt")        
+                for count, (artist, song, u) in enumerate(zip(store_artist, store_song, uris),start=1):
+                    with open(logFile, 'a', encoding='utf-8') as f:
+                        if f.tell()==0:
+                            f.write(f"{ff}\nauthor: {author}\nplaylist: {title}\nURL: {ttp}\n\n")
+                        f.write(f"Number: {count}\n{artist} • {song}\n{u}\n\n")
+        APIget(token)
+    except:
+        Failed_export.append(ttp)
+        print(f"\nerror while loading data from {ttp}\n")
