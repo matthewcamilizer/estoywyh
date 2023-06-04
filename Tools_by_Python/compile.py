d12aa5e68032ce84eb3607f5356a28a510e1fffe
matthewcamilizer <50293364+matthewcamilizer@users.noreply.github.com>
2023-06-04 15:20:55 +0800

    u

--- Tools_by_Python/compile.py
+++ Tools_by_Python/compile.py
@@ -4,31 +4,39 @@ from exportSpotify import ExportSpotify
 from NCM import ExportNCM
 from QQ import ExportQQ
 
-
-
-play_list=input("你要种什么歌单? ")
+play_lists=input("你要种什么歌单? 如果是多个请用逗号隔开 ")
 save_path=input("想保存在哪个文件夹的花园里? ")
-er=["只能用网易云音乐/QQ音乐/Spotify的歌单链接, 其他的都种不出花的", "只能用网易云音乐/QQ音乐/Spotify的歌单呢! 其他的在火星上还没摸不到", 
-"⊙ ⁠_ ⁠◎ ⁠)这啥啊我不认得啊? 我只要网易云音乐/QQ音乐/Spotify的"]
+er=["楼上是谁啊???只能用网易云音乐/QQ音乐/Spotify的歌单链接, 其他的都种不出花的", "上面是火星来的链接吗, 只能用网易云音乐/QQ音乐/Spotify的歌单呢!", 
+"⊙ ⁠_ ⁠◎ ⁠)这玩意是啥啊我不认得啊? 我只要网易云音乐/QQ音乐/Spotify的^_^"]
 
 pattern=r"https://([^/]+)"
-try:
-    get=re.search(pattern, play_list).group(1)
+pl= "".join(play_lists.split()).split(",")
+QQ_export=[]
+NCM_export=[]
+Failed_export=[]
+Spotify_export=[]
+Ons=[]
 
-    if "y.qq.com" in get:
-        ExportQQ(play_list, save_path)
-    elif "open.spotify.com" in get:
-        ExportSpotify(play_list, save_path)
-    elif "music.163.com" in get:
-        ExportNCM(play_list, save_path)  
-    else:
-        print(f"{play_list}\n{er[random.randint(0, len(er)-1)]}")
-except:
-    print(f"{play_list}\n{er[random.randint(0, len(er)-1)]}")
+for play_list in pl:
+    print("等一下...")
+    try:
+        get=re.search(pattern, play_list).group(1)
+        if "y.qq.com" in get:
+            ExportQQ(play_list, save_path, QQ_export, Ons, Failed_export)
+        elif "open.spotify.com" in get:
+            ExportSpotify(play_list, save_path, Spotify_export, Ons, Failed_export)
+        elif "music.163.com" in get:
+            ExportNCM(play_list, save_path, NCM_export, Ons, Failed_export)  
+        else:
+            print(f"\n{play_list}\n{er[random.randint(0, len(er)-1)]}\n")
+    except:
+        print(f"\n{play_list}\n{er[random.randint(0, len(er)-1)]}\n")
 
-
-
-
-
-
-
+if len(Failed_export)==0 and len(pl) > 0:
+    wh=["终于无差错全部导出, 累死我了, 给你搞这么多","还行, 如果ZZY知道我一次能不出错完成这么多, 肯定会把黑名单给我解了!",
+    "好了好了, 没有报错, 回家睡觉"]
+    print(f"{wh[random.randint(0, len(wh)-1)]}")
+if len(Failed_export)!=0:
+    for fa in Failed_export:
+        print(f"\n{fa}\n")
+    print(f"\n这些歌单链接有问题, 看看是不是输错了, 或者是部分歌曲下架了\n") 
