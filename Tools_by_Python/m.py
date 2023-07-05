02f7d3c044f272a309893769a98741be410c9244
matthewcamilizer <matthewhilmi666@gmail.com>
2023-07-06 05:10:16 +0800

    u

--- Tools_by_Python/m.py
+++ Tools_by_Python/m.py
@@ -1,34 +1,42 @@
 #pip install mysql-connector-python
 import mysql.connector
 
-host=input("Enter SQL host: ")
-user=input("Enter SQL user: ")
-pw=input("Enter SQL password: ")
-db=input("Enter SQL database: ")
+def tomysql():
+  user=input("Enter SQL user: ")
+  host=input("Enter SQL host: ")
+  pw=input("Enter SQL password: ")
+  mydb = mysql.connector.connect(
+    host=host,
+    user=user,
+    password=pw
+  )
+  print(f"Succesfully connected to {user}@{host}")
 
+  # Create a cursor object to interact with the database
+  mycursor = mydb.cursor()
+  com=input("Enter SQL commands: ")
+  # Execute a SQL query
+  mycursor.execute(com)
+
+  # Fetch all the rows in the result set
+  result = mycursor.fetchall()
+  # Print the rows
+  for row in result:
+    print(row)
+  
 while True:
   try:
-    # Connect to the MySQL database
-    mydb = mysql.connector.connect(
-      host=host,
-      user=user,
-      password=pw,
-      database=db
-    )
-    print(f"Succesfully connected to {host}.{user}.{db}")
-
-    # Create a cursor object to interact with the database
-    mycursor = mydb.cursor()
-    com=input("Enter SQL commands: ")
-# Execute a SQL query
-    mycursor.execute(com)
-
-# Fetch all the rows in the result set
-    result = mycursor.fetchall()
-# Print the rows
-    for row in result:
-      print(row)
-    
-  except:
-    print(f"Failed to connected to {host}.{user}.{db}")
-    break  
+    tomysql()
+  
+    To=input("y?\n").lower()
+    if To == 'y':
+      continue
+    else:
+      break
+  except Exception as e:
+    print(e)
+    To=input("y?\n").lower()
+    if To == 'y':
+      continue
+    else:
+      break
