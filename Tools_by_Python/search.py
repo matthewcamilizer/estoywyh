5dbc01212211dbce3465228bf01e323bf49159eb
matthewcamilizer <50293364+matthewcamilizer@users.noreply.github.com>
2023-08-09 01:16:18 +0800

    u

---
+++ Tools_by_Python/search.py
@@ -0,0 +1,40 @@
+import requests, re, sys, json
+sys.dont_write_bytecode=True
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+from dotenv import dotenv_values
+
+c={
+    **dotenv_values(".env.zzyily")
+}
+
+client_id=c['sp_cid']
+client_secret=c['sp_cs']
+
+def get_ncm(song_name):
+    a=requests.get(f"https://music.163.com/api/search/get/?csrf_token=hlpretag=&hlposttag=&s={song_name}&type=1&offset=0&total=true&limit=20", headers=h).text
+    b=json.loads(a)
+    result=b['result']['songs']
+    song=f"https://music.163.com/song?id={result[0]['id']}"
+    song_inapp=f"https://y.music.163.com/m/song?id={result[0]['id']}"
+
+    return song,song_inapp
+
+def get_Spotify(song_name):
+    # Set up the authentication credentials
+    credentials = SpotifyClientCredentials(client_id=client_id,client_secret=client_secret,)
+    sp = spotipy.Spotify(client_credentials_manager=credentials)
+
+    results = sp.search(q=song_name, type='track', limit=1)
+    return results['tracks']['items'][0]['external_urls']['spotify'], results['tracks']['items'][0]['uri']
+
+h = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36', 'cookie': "b_nut=1691327560;b_ut=7;buvid3=A6160ADF-167A-5909-DC9B-968637F1F9D260792infoc;i-wanna-go-back=-1;innersign=0"}
+def get_bilibili(song_name):
+    a=requests.get(f"https://api.bilibili.com/x/web-interface/search/all/v2?page=1&keyword={song_name}", headers=h).text
+    b=json.loads(a)
+
+    bili=b['data']['result'][11]['data'][0]['arcurl']
+    pattern=r"/video/av([^\W]+)"
+    aid=re.search(pattern, bili).group(1)
+    ebili=f"https://player.bilibili.com/player.html?aid={aid}&muted=false&autoplay=false"
+
+    return bili, ebili
