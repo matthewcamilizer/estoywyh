e874feffc334eb972db0b9321cc69c7dcd3a6da7
matthewcamilizer <50293364+matthewcamilizer@users.noreply.github.com>
2023-08-18 11:54:22 +0800

    u

--- Tools_by_Python/search.py
+++ Tools_by_Python/search.py
@@ -44,7 +44,8 @@ yt_api=c['yt_api']
 def search_youtube(song):
     youtube = googleapiclient.discovery.build(serviceName='youtube', version='v3', developerKey=yt_api)
     res=youtube.search().list(part='snippet', q=f"{song} (audio)", maxResults=1, type='video')
-    resp=res.execute()
-    vid = resp['items'][0]['id']['videoId']
-    response=f"https://youtu.be/{vid}"
-    return response
+<<<<<<< HEAD
+=======
+    response=res.execute()
+    return return f"https://youtu.be/{response['items'][0]['id']['videoId']}"
+>>>>>>> 6abd1081ce4b0010de4b342d2fe821bcc5c58e56
