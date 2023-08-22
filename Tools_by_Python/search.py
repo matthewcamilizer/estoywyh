3b4a7950b444ac545c8c1d454cb8c73b6ebf4ed3
matthewcamilizer <50293364+matthewcamilizer@users.noreply.github.com>
2023-08-22 13:31:31 +0800

    u

--- Tools_by_Python/search.py
+++ Tools_by_Python/search.py
@@ -13,13 +13,16 @@ client_secret = c['sp_cs']
 h = c['header']
 
 def get_ncm(song_name):
+    """_
+    song, song in-app, artist
+    """
     a=requests.get(f"https://music.163.com/api/search/get/?csrf_token=hlpretag=&hlposttag=&s={song_name}&type=1&offset=0&total=true&limit=20", headers=h).text
     b=json.loads(a)
     result=b['result']['songs']
     song=f"https://music.163.com/song?id={result[0]['id']}"
     song_inapp=f"https://y.music.163.com/m/song?id={result[0]['id']}"
-
-    return song,song_inapp
+    artist=f"https://music.163.com/artist?id={result[0]['artists'][0]['id']}"
+    return song,song_inapp, artist
 
 def get_Spotify(song_name):
     # Set up the authentication credentials
@@ -37,12 +40,40 @@ def get_bilibili(song_name):
     pattern=r"/video/av([^\W]+)"
     aid=re.search(pattern, bili).group(1)
     ebili=f"https://player.bilibili.com/player.html?aid={aid}&muted=false&autoplay=false"
+    
+    bilibili=bili.replace("http://","https://")
 
-    return bili, ebili
+    return bilibili, ebili
 
 yt_api=c['yt_api']
-def search_youtube(song):
-    youtube = googleapiclient.discovery.build(serviceName='youtube', version='v3', developerKey=yt_api)
-    res=youtube.search().list(part='snippet', q=f"{song} (audio)", maxResults=1, type='video')
+def search_youtube(song_name,n):
+    youtube = googleapiclient.discovery.build(serviceName='youtube', version='v3', developerKey=api)
+    res=youtube.search().list(part='snippet', q=song_name, maxResults=n, type='video')
     response=res.execute()
-    return f"https://youtu.be/{response['items'][0]['id']['videoId']}"
+
+    vid=[]
+    channel=[]
+    title=[]
+    date=[]
+    author=[]
+    img=[]
+    raw={}
+
+
+    for i in response['items']:
+        title.append(i['snippet']['title'])
+        author.append(i['snippet']['channelTitle'])
+        date.append(i['snippet']['publishTime'])
+        vid.append(f"youtu.be/{i['id']['videoId']}")
+        channel.append(f"youtube.com/channel/{i['snippet']['channelId']}")
+        img.append(i['snippet']['thumbnails']['medium']['url'])
+
+    for index, (t,a,d,v,c,i) in enumerate(zip(title, author, date, vid, channel, img), start=1):
+        raw.update({f"video{index}":{"title":t,"author":a, "date":d, "vid":v, "channel":c, "img":i}})
+
+    return raw
+
+async def recognize_song(f):
+    shazam = Shazam()
+    out = await shazam.recognize_song(f)
+    return out
