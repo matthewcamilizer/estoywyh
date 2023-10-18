02a3f3d921225507790df9282163cd136da713ad
matthewcamilizer <matthewhilmi666@gmail.com>
2023-10-18 21:41:05 +0800

    hw

---
+++ leetcode/extract_longest_non_adjacent_chrs.py
@@ -0,0 +1,43 @@
+a=input("enter a string:")
+l=[]
+s={}
+
+start=[]
+end=[]
+gl=[]
+if len(a)==1:
+    gl.append(1)
+elif len(a)==2:
+    if all(x==a[0] for x in a):
+        gl.append(1)
+    else:
+        gl.append(2)
+else:
+    if all(x==a[0] for x in a):
+        gl.append(1)
+    else:
+        # add 0 if first two are the same
+        if a[0]==a[1]:
+            start.insert(0,0)
+        for i in range(1,len(a)-1):
+            if not a[i-1]==a[i] and a[i]==a[i+1]:
+                start.append(i)
+
+        for i in range(2,len(a)):
+            if a[i-2]==a[i-1] and not a[i-1]==a[i]:
+                end.append(i-1)
+        if a[len(a)-1]==a[len(a)-2]:
+            end.append(len(a)-1)
+
+        for i in range(len(start)-1):
+            gl.append(start[i+1]-end[i]+1)
+        for i in range(len(start)):
+            s[i]=[start[i],end[i]]
+
+print(f"dict for index of adjacent chrs: {s}")
+for i in range(len(start)):
+    print(f"the {i} adjacent: {a[s[i][0]:s[i][1]+1]}")
+for i in range(len(start)-1):
+    print(f"the {i} non-adjacent: {a[end[i]:start[i+1]+1]}")
+
+print(f"length of the longest non-adjacent chrs: {max(gl)}")
---
+++ leetcode/extract_longest_non_duplicated_chrs.py
@@ -0,0 +1,32 @@
+a=input("enter a string:")
+l=[]
+s={}
+
+def non_repeat(a,l):
+    for i in range(len(a)):
+        for j in range(i):
+            if a[j]==a[i]:
+                if [i,j] not in l:
+                    l.append(i)
+
+    # get the start index of the next duplicate element
+    l=list(sorted(set(l)))
+    if len(a) not in l:
+        l.append(len(a))
+
+    print(l)
+    difs=[]
+    if len(l)==1:
+        difs.append(l[0])
+    elif len(l)==2:
+        difs.append(l[1]-len[0])
+    else:
+        for i in range(2,len(l)):
+            dif=l[i]-l[i-2]
+            if dif not in difs:
+                difs.append(dif)
+    print(difs)
+    return max(difs), a[l[i-2]:l[i]]
+
+o=non_repeat(a,l)
+print(f"the longest chrs without duplication is {o[1]}, length: {o[0]}")
