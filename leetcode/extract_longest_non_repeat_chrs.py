3dff0ffd5bcc2dd128a725d767ae72d08b518270
matthewcamilizer <matthewhilmi666@gmail.com>
2023-10-18 13:30:45 +0800

    hw

--- leetcode/extract_longest_non_repeat_chrs.py
+++ leetcode/extract_longest_non_repeat_chrs.py
@@ -23,8 +23,8 @@ else:
         for i in range(2,len(a)):
             if a[i-2]==a[i-1] and not a[i-1]==a[i]:
                 end.append(i-1)
-            if a[len(a)-1]==a[len(a)-2]:
-                end.append(len(a)-1)
+        if a[len(a)-1]==a[len(a)-2]:
+            end.append(len(a)-1)
 
         for i in range(len(start)):
             gl.append(end[i]-start[i]+1)
